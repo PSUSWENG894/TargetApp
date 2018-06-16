f76ca9ddcce22f7846cc90b0e8a9d06d1b3d8df9
Alex Payne <jap91@psu.edu>
2018-06-15 21:39:38 -0400

    Test some GitHub pages, cannot get Travis CI to add these  files

---
+++ precache-manifest.f8702efb7f96291d62022050686a4340.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c0d02c339491cfe370e",
+    "url": "/js/vendors~app.3c0d02c3.js"
+  },
+  {
+    "revision": "6afd0f22ead2d0f0af04",
+    "url": "/js/app.6afd0f22.js"
+  },
+  {
+    "revision": "e87fdd8e3b13695fe2359a568d76ee59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c0d02c339491cfe370e",
+    "url": "/css/vendors~app.0.44d2450f.css"
+  },
+  {
+    "revision": "6afd0f22ead2d0f0af04",
+    "url": "/css/app.bc15108b.css"
+  }
+];
