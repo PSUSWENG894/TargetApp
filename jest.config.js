5da638b8ecb9c3136c0629aa4cef17caf5b4edd0
Alex Payne <jap91@psu.edu>
2018-06-17 12:00:16 -0400

    Trying to fix jest test, but cannot get tests to work properly. Re-adding test to travis-ci, will fail the build.

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,23 @@
-export const moduleFileExtensions = [
-  'js',
-  'jsx',
-  'json',
-  'vue'
-];
-export const transform = {
-  '^.+\\.vue$': 'vue-jest',
-  '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
-  '^.+\\.jsx?$': 'babel-jest'
-};
-export const moduleNameMapper = {
-  '^@/(.*)$': '<rootDir>/src/$1'
-};
-export const snapshotSerializers = [
-  'jest-serializer-vue'
-];
-export const testMatch = [
-  '<rootDir>/(tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx))'
-];
+/* eslint-disable */
+module.exports = {
+  moduleFileExtensions: [
+    'js',
+    'jsx',
+    'json',
+    'vue'
+  ],
+  transform: {
+    '^.+\\.vue$': 'vue-jest',
+    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+    '^.+\\.jsx?$': 'babel-jest'
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  snapshotSerializers: [
+    'jest-serializer-vue'
+  ],
+  testMatch: [
+    '<rootDir>/(tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx))'
+  ]
+}
