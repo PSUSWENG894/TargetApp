97afc76f819c72a5852599c578b610d191da4acd
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-06-18 02:00:45 +0000

    Deploy PSUSWENG894/TargetApp to github.com/PSUSWENG894/TargetApp.git:gh-pages

--- config/prod.env.js
+++
@@ -1,3 +0,0 @@
-export const NODE_ENV = 'dev';
-export const debugMOde = true;
-export const apiURL = 'https://api.travis-ci.org/repo';
---
+++ precache-manifest.72d3d9ed4df9f6c6dc12ffd5a529c9ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8af2ca30d3a46e3f3955",
+    "url": "/js/vendors~app.8af2ca30.js"
+  },
+  {
+    "revision": "0a4973afe16e8eb912fb",
+    "url": "/js/app.0a4973af.js"
+  },
+  {
+    "revision": "f28afdbb896362c581f8622942bf49c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8af2ca30d3a46e3f3955",
+    "url": "/css/vendors~app.0.884fcde2.css"
+  },
+  {
+    "revision": "0a4973afe16e8eb912fb",
+    "url": "/css/app.d51fd75b.css"
+  }
+];
