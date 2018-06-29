e4bc2e4df22ba40031936cb76e6c8cac34dc8e97
georgemgeorge <georgemgeorge@gmail.com>
2018-06-28 19:54:47 -0500

    Unit test for GitHubAPI
    Added a unit test suite for GitHubInformation Component

---
+++ tests/unit/GitHubInformation.spec.js
@@ -0,0 +1,28 @@
+jest.mock('axios', () => require('./axios.mock'));
+
+import Vue from 'vue'
+import GitHubInformation from '@/components/GitHubInformation'
+import axios from 'axios'
+import { mount } from '@vue/test-utils'
+
+describe('GitHubInformation.vue', () => {
+
+    it('should render correct contents', () => {
+        const Constructor = Vue.extend(GitHubInformation)
+        const vm = new Constructor().$mount()
+        expect(vm).toBeTruthy()
+    });
+
+    it('test setData', () => {
+        const response =
+            [
+                {name: 'test'},
+                {name: 'test2'},
+                {name: 'test3'}
+            ];
+
+        const wrapper = mount(GitHubInformation)
+        wrapper.vm.setData(response)
+        expect(wrapper.vm.info).toEqual(response);
+    });
+});
