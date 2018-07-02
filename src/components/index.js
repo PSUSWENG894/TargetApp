4442643e95b977fc14d10643811f4257f6a43847
Alex Payne <jap91@psu.edu>
2018-07-01 21:48:58 -0400

    Refactor for linting and vue-router.

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,12 @@ import PassFailChart from './PassFailChart';
 import BuildInformation from './BuildInformation'
 import Repositories from './Repositories'
 import BuildButton from './BuildButton'
+import Authorization from './Authorization.vue'
 
 export {
     PassFailChart,
     BuildInformation,
     Repositories,
-    BuildButton
+    BuildButton,
+    Authorization
 }
--- src/main.js
+++ src/main.js
@@ -1,11 +1,39 @@
 import Vue from 'vue'
+
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/black-green-dark.css' // This line here
 Vue.use(VueMaterial)
 
+import VueRouter from 'vue-router'
+import {
+  Authorization,
+  Repositories
+} from './components'
+const routes = [{
+    path: '',
+    component: Authorization
+  },
+  {
+    path: '/dashboard',
+    name: 'dashboard',
+    component: Repositories,
+    props: true
+  }
+]
+
+// 3. Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+const router = new VueRouter({
+  routes // short for `routes: routes`
+})
+
+Vue.use(VueRouter)
+
 import App from './App.vue'
 Vue.config.productionTip = false
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router: router
 }).$mount('#app')
