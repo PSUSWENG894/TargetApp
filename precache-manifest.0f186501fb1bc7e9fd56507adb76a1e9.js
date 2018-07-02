418eb7b026bf3754431d7a9fb6595ee0213117ea
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-07-02 23:56:31 +0000

    Deploy PSUSWENG894/TargetApp to github.com/PSUSWENG894/TargetApp.git:gh-pages

--- precache-manifest.0f186501fb1bc7e9fd56507adb76a1e9.js
+++ precache-manifest.0f186501fb1bc7e9fd56507adb76a1e9.js
@@ -4,11 +4,11 @@ self.__precacheManifest = [
     "url": "js/vendors~app.fa6f1f99.js"
   },
   {
-    "revision": "fbf563b24e51fe6a54b9",
-    "url": "js/app.fbf563b2.js"
+    "revision": "50f87e7eeb8b9c162ec4",
+    "url": "js/app.50f87e7e.js"
   },
   {
-    "revision": "dfee073bdd34e56e462c6d8c575f8da5",
+    "revision": "ba69df90240c6e119db53c1f04a8c2e6",
     "url": "index.html"
   },
   {
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "css/vendors~app.0.86fed64d.css"
   },
   {
-    "revision": "fbf563b24e51fe6a54b9",
+    "revision": "50f87e7eeb8b9c162ec4",
     "url": "css/app.fed3d174.css"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.594d32cf80a7bc481e29fcd2f6634811.js"
+  "precache-manifest.0f186501fb1bc7e9fd56507adb76a1e9.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "target-app"});
