79d5390947d32df8743a1ce895ba3aeab8877181
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-07-07 02:41:13 +0000

    Deploy PSUSWENG894/TargetApp to github.com/PSUSWENG894/TargetApp.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.0aacea58e4cd32012df49a05d4c7709c.js"
+  "precache-manifest.cb63c1b5516e60fbff0aa4bb11360ead.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "target-app"});
