24ce72e02ed546ee72ad332615f825a14e498c83
Steven Ford <evensteven01@gmail.com>
2018-07-10 23:24:30 -0400

    Cleaned up the code a bit. Removed comments

--- src/components/DeveloperContributions.vue
+++ src/components/DeveloperContributions.vue
@@ -1,29 +1,5 @@
 <template>
     <div class="display-items">
-        <!-- <md-list v-if="loaded" id="linesPerDev" v-for="authorContributions in info" v-bind:key="authorContributions.author">
-            <md-list-item>
-                <div class="md-list-item-text">
-                    <md-card>
-                        <md-card-header>
-                            <md-card-header-text>
-                                <div class="md-title">{{authorContributions.author}}</div>
-                            </md-card-header-text>
-                        </md-card-header>
-                        <md-card-content>
-                            <md-list id="authorContributionList" v-for="repoContributions in authorContributions.contributions">
-                                <md-list-item>
-                                    <div class="md-list-item-text">
-                                        <span>Repositoy: {{repoContributions.repository}}</span>
-                                        <span>Total: {{repoContributions.contribution.total}}</span>
-                                    </div>
-                                </md-list-item>
-                            </md-list>
-                        </md-card-content>
-                    </md-card>
-                </div>
-            </md-list-item>
-        </md-list> -->
-
         <div v-if="loaded" id="aaaaaalinesPerDev" v-for="authorContributions in info" v-bind:key="authorContributions.author">
             <md-card>
                 <md-card-header>
@@ -47,8 +23,6 @@
                 </md-card-content>
             </md-card>
         </div>
-        
-
   </div>
 </template>
 
@@ -74,7 +48,6 @@
         },
         methods: {
             async fetchData() { ///repos/:owner/:repo/stats/contributors
-                //const repo = 'TargetApp'
                 const repositoryList = ['TargetApp', 'BudgetAPI'];
                 const repo = repositoryList[0];
 
@@ -96,19 +69,12 @@
                 return repoPromiseList;
             },
             organizeData(promiseList, results) {
-                console.log('Promises');
-                console.log(promiseList);
-                console.log('Results');
-                console.log(results);
-
                 const dictByAuthor = {}                
                 results.forEach((result, index) => {
 
                     result.forEach(contribution => {
                         const repo = promiseList[index].repo
                         const author = contribution.author.login;
-                        console.log('Repo: ' + repo);
-                        console.log('Author: ' + author);
 
                         const authorContributions = dictByAuthor[author] ? dictByAuthor[author] : []
                         authorContributions.push({'repository': repo, 'contribution': contribution})
@@ -116,28 +82,15 @@
                     });
                 });
 
-                //Collapse it
+                //Now package it in a nice JSON
                 let organizedData = []
                 Object.keys(dictByAuthor).forEach(author => {
                     organizedData.push({'author': author, 'contributions': dictByAuthor[author]})
                 });
 
-                console.log('Total results');
-                console.log(dictByAuthor)
-                console.log('Collapsed to');
-                console.log(organizedData)
                 return organizedData;
             },
             setData(theData) {
-                console.log('Lines per dev data:');
-                console.log(theData);
-
-                const newList = [];
-                // theData.forEach(contribution => {
-                //     console.log('Contribution');
-                //     console.log(contribution.author.login);
-                // });
-
                 this.info = theData;
                 this.loaded = true
             }
