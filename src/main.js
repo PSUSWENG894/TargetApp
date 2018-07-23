12056a88d66b21c35ec5e85225604644be851e06
Alex Payne <jap91@psu.edu>
2018-07-22 22:07:14 -0400

    Readded build all button

--- src/main.js
+++ src/main.js
@@ -32,7 +32,7 @@ const routes = [{
   }
 ]
 const router = new VueRouter({
-  // mode: 'history',
+  // mode: 'history', // Github Pages does not handle History Mode Correctly
   routes: routes
 })
 
